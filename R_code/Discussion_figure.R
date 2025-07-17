d95a50881acea3fd1dbdc1d9c0aaf535327ea1b8
Alexandra Bloecker <alexandrambloecker@gmail.com>
2025-07-17 15:11:08 +0200

    created Sankey plot for discussion.

--- R_code/Discussion_figure.R
+++ R_code/Discussion_figure.R
@@ -4,6 +4,51 @@ library(readxl)
 # 1. Data
 dat <- read_excel("Data/Table_Discussion.xlsx")
 
+# Names without _
+alternative_n <- c(reduce_fertilization= "reduce fertilization",
+          riparian_strips = "riparian strips",
+          GES = "GES",
+          EBFM = "EBFM" ,
+          non_invasive_monitoring ="non-invasive monitoring",
+          fisheries_monitoring = "fisheries monitoring",
+          ICES_advice_system = "ICES advice system",
+          reduce_vessels = "reduce vessels",
+          new_quota_distribution = "new quota distribution",
+          ban_trawling = "ban trawling",
+          educational_programs = "educational programs",
+          environm_friendly_gear = "environmentally friendly gear",
+          searanger = "Sea Ranger",
+          subsidies = "subsidies",
+          diversification = "diversification",
+          re_training = "re-training",
+          MPAs = "MPAs",
+          no_take_areas = "no take areas",
+          promote_trourism = "promote trourism",
+          alternative_marketing = "alternative marketing",
+          top_predator_reduction = "top predator reduction",
+          coastal_infrastructure = "coastal infrastructure")
+
+objective_n <- c(healthy_ecosystem = "healthy ecosystem",
+                 protect_stock = "protect stock",
+                 s_f_support_livelihoods ="support livelihoods (SF)",
+                 safeguard_coastal_pop_well_being = "sageguard well-being",
+                 preserve_cultural_value = "preserve cultural value",
+                 s_f_economically_viable = "economically viable (SF)",
+                 s_f_environmentally_friendly = "environmentally friendly (SF)")
+
+SES_n <- c(S_AS_socio_economic = "S-AS socio-economic",
+           S_AS_scientific_advice = "S-AS scientific advice",
+           E_AS_predation = "E-AS predation",
+           PE_AS_abiotic = "PE-AS abiotic",
+           S_AS_rule_making = "S-AS rule making",
+           SE_AS_recreational = "SE-AS recreational",
+           SE_AS_harvesting = "SE-AS harvesting")
+
+dat$Alternatives <- as.character(alternative_n[dat$Alternatives])
+dat$Objectives <- as.character(objective_n[dat$Objectives])
+dat$SES_AS <- as.character(SES_n[dat$SES_AS])
+
+#1.Objectives in center----
 # 2. Colours
 n <- length(unique(dat$Objectives))
 pal <- unlist(mapply(RColorBrewer::brewer.pal, 9, 'Set1'))
@@ -18,7 +63,7 @@ dom <- paste(shQuote(dom), collapse = ", ")
 
 my_color <- paste0("d3.scaleOrdinal().domain([", dom, ",'nodes']).range([", pal, ",'grey'])")
 
-#1.Objectives in center----
+
 # 3. Links
 temp <- dat %>%
   group_by(Alternatives, Objectives, SES_AS) %>%
@@ -71,6 +116,21 @@ sankey_traits_plot
 saveWidget(sankey_traits_plot, "sankey_traits_plot_objectives.html")
 
 #2.SES_AS in center----
+# 2. Colours
+n <- length(unique(dat$SES_AS))
+pal <- unlist(mapply(RColorBrewer::brewer.pal, 9, 'Set1'))
+pal2 <- unlist(mapply(RColorBrewer::brewer.pal, 5, 'Set2'))
+pal3 <- unlist(mapply(RColorBrewer::brewer.pal, 6, 'Set3'))
+pal <- c(pal, pal2, pal3)
+pal <- paste(shQuote(pal[1:n]), collapse = ", ")
+
+pal <- c( "'#E41A1C', '#377EB8', '#4DAF4A', '#C1CDCD', '#FF7F00', '#FFFF33'")
+
+# Domain for Colours
+dom <- unique(dat$SES_AS)
+dom <- paste(shQuote(dom), collapse = ", ")
+
+my_color <- paste0("d3.scaleOrdinal().domain([", dom, ",'nodes']).range([", pal, ",'grey'])")
 # 3. Links
 temp <- dat %>%
   group_by(Alternatives, SES_AS, Objectives) %>%
@@ -121,3 +181,4 @@ sankey_traits_plot <- htmlwidgets::prependContent(
 sankey_traits_plot
 
 saveWidget(sankey_traits_plot, "sankey_traits_plot_SES_AS.html")
+
