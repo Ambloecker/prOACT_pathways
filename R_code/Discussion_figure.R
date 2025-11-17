36e1811e828c4fce973eefff7cf86eab48c35841
Ambloecker <alexandrambloecker@gmail.com>
2025-11-17 14:50:37 +0100

    updated Sankey figure.

--- R_code/Discussion_figure.R
+++ R_code/Discussion_figure.R
@@ -51,70 +51,70 @@ dat$SES_AS <- as.character(SES_n[dat$SES_AS])
 
 #1.Objectives in center----
 # 2. Colours
-n <- length(unique(dat$Objectives))
-pal <- unlist(mapply(RColorBrewer::brewer.pal, 9, 'Set1'))
-pal2 <- unlist(mapply(RColorBrewer::brewer.pal, 5, 'Set2'))
-pal3 <- unlist(mapply(RColorBrewer::brewer.pal, 6, 'Set3'))
-pal <- c(pal, pal2, pal3)
-pal <- paste(shQuote(pal[1:n]), collapse = ", ")
-
-# Domain for Colours
-dom <- unique(dat$Objectives)
-dom <- paste(shQuote(dom), collapse = ", ")
-
-my_color <- paste0("d3.scaleOrdinal().domain([", dom, ",'nodes']).range([", pal, ",'grey'])")
-
-
-# 3. Links
-temp <- dat %>%
-  group_by(Alternatives, Objectives, SES_AS) %>%
-  count()
-
-# Sub_driver -> Life_history_traits
-links1 <- temp %>%
-  transmute(source = Alternatives, target = Objectives, value = n, linkgroup = Objectives)
-
-# Life_history_traits -> Live_stage
-links2 <- temp %>%
-  transmute(source = Objectives, target = SES_AS, value = n, linkgroup = Objectives)
-
-# combine
-sankey_links <- bind_rows(links1, links2)
-
-# 4. Knots
-nodes <- data.frame(name = unique(c(sankey_links$source, sankey_links$target)))
-nodes$group <- "nodes"
-
-# IDs
-sankey_links$IDsource <- match(sankey_links$source, nodes$name) - 1
-sankey_links$IDtarget <- match(sankey_links$target, nodes$name) - 1
-
-# 5. Sankey-Plot
-sankey_traits_plot <- networkD3::sankeyNetwork(
-  Links = sankey_links,
-  Nodes = nodes,
-  Source = "IDsource",
-  Target = "IDtarget",
-  Value = "value",
-  NodeID = "name",
-  fontSize = 14,
-  fontFamily = 'Ubuntu',
-  height = 700,
-  width = 1000,
-  colourScale = my_color,
-  LinkGroup = "linkgroup",
-  NodeGroup = "group"
-)
-
-# 6. Title
-sankey_traits_plot <- htmlwidgets::prependContent(
-  sankey_traits_plot,
-  htmltools::tags$h5("Pathways in SES")
-)
-
-sankey_traits_plot
-
-saveWidget(sankey_traits_plot, "sankey_traits_plot_objectives.html")
+# n <- length(unique(dat$Objectives))
+# pal <- unlist(mapply(RColorBrewer::brewer.pal, 9, 'Set1'))
+# pal2 <- unlist(mapply(RColorBrewer::brewer.pal, 5, 'Set2'))
+# pal3 <- unlist(mapply(RColorBrewer::brewer.pal, 6, 'Set3'))
+# pal <- c(pal, pal2, pal3)
+# pal <- paste(shQuote(pal[1:n]), collapse = ", ")
+# 
+# # Domain for Colours
+# dom <- unique(dat$Objectives)
+# dom <- paste(shQuote(dom), collapse = ", ")
+# 
+# my_color <- paste0("d3.scaleOrdinal().domain([", dom, ",'nodes']).range([", pal, ",'grey'])")
+# 
+# 
+# # 3. Links
+# temp <- dat %>%
+#   group_by(Alternatives, Objectives, SES_AS) %>%
+#   count()
+# 
+# # Sub_driver -> Life_history_traits
+# links1 <- temp %>%
+#   transmute(source = Alternatives, target = Objectives, value = n, linkgroup = Objectives)
+# 
+# # Life_history_traits -> Live_stage
+# links2 <- temp %>%
+#   transmute(source = Objectives, target = SES_AS, value = n, linkgroup = Objectives)
+# 
+# # combine
+# sankey_links <- bind_rows(links1, links2)
+# 
+# # 4. Knots
+# nodes <- data.frame(name = unique(c(sankey_links$source, sankey_links$target)))
+# nodes$group <- "nodes"
+# 
+# # IDs
+# sankey_links$IDsource <- match(sankey_links$source, nodes$name) - 1
+# sankey_links$IDtarget <- match(sankey_links$target, nodes$name) - 1
+# 
+# # 5. Sankey-Plot
+# sankey_traits_plot <- networkD3::sankeyNetwork(
+#   Links = sankey_links,
+#   Nodes = nodes,
+#   Source = "IDsource",
+#   Target = "IDtarget",
+#   Value = "value",
+#   NodeID = "name",
+#   fontSize = 14,
+#   fontFamily = 'Ubuntu',
+#   height = 700,
+#   width = 1000,
+#   colourScale = my_color,
+#   LinkGroup = "linkgroup",
+#   NodeGroup = "group"
+# )
+# 
+# # 6. Title
+# sankey_traits_plot <- htmlwidgets::prependContent(
+#   sankey_traits_plot,
+#   htmltools::tags$h5("Pathways in SES")
+# )
+# 
+# sankey_traits_plot
+# 
+# saveWidget(sankey_traits_plot, "sankey_traits_plot_objectives.html")
 
 #2.SES_AS in center----
 # 2. Colours
@@ -125,7 +125,8 @@ pal3 <- unlist(mapply(RColorBrewer::brewer.pal, 6, 'Set3'))
 pal <- c(pal, pal2, pal3)
 pal <- paste(shQuote(pal[1:n]), collapse = ", ")
 
-pal <- c( "'#E41A1C', '#377EB8', '#4DAF4A', '#C1CDCD', '#FF7F00', '#FFFF33'")
+pal <- c( "'#E41A1C', '#21e3fc', '#FF7F00', '#C1CDCD', '#37fa44', '#FFFF33'")
+#pal <- c( "#E41A1C", '#21e3fc', '#FF7F00', '#C1CDCD', '#37fa44', '#FFFF33')
 
 # Domain for Colours
 dom <- unique(dat$SES_AS)
@@ -181,5 +182,5 @@ sankey_traits_plot <- htmlwidgets::prependContent(
 
 sankey_traits_plot
 
-saveWidget(sankey_traits_plot, "sankey_traits_plot_SES_AS.html")
+#saveWidget(sankey_traits_plot, "sankey_traits_plot_SES_AS.html")
 
